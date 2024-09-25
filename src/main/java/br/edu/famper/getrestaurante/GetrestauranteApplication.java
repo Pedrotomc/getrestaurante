ea5f8ed75c9f6c462d3718d5186c079647934cf8
Pedrotomc <pedrotomczyk34@gmail.com>
2024-09-25 20:13:06 -0300

    Initial commit

---
+++ src/main/java/br/edu/famper/getrestaurante/GetrestauranteApplication.java
@@ -0,0 +1,13 @@
+package br.edu.famper.getrestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetrestauranteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetrestauranteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/famper/getrestaurante/GetrestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.famper.getrestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetrestauranteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
